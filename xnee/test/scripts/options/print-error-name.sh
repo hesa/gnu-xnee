22493f6ba3237bb41d71c64906f7d6df3f4c46df
hesa <hesa>
2003-10-23 20:46:41 +0000

    scripts for testing error options

---
+++ xnee/test/scripts/options/print-error-name.sh
@@ -0,0 +1,221 @@
+#/bin/sh
+#
+#
+# Filename:   options/print-error-name.sh
+# Generated:  
+# 
+# Description:
+#
+#  Test the Xnee option
+#       --print-error-name
+#  By reading up X11 error (name and number) from
+#  the header file X.h. These values are compared
+#  with the output from Xnee 
+#
+#
+ 
+ 
+ 
+ 
+ 
+# name of myself
+MYNAME=print-error-name.sh
+ 
+# Arguments to test (short and long)
+export LONG_ARG=--print-error-name
+export SHORT_ARG=-pern
+ 
+# source useful funs
+if [ ! -f etc/base_funs ]; 
+then 
+    echo "Can't find the file: base_funs"  
+    echo "Start me up in the xnee test dir "
+    exit
+else 
+    . etc/base_funs
+fi 
+ 
+##init_test 
+parse_me "$*"
+
+#######################################################################
+
+
+X11_NAMES=/tmp/xnee_errors.tmp
+ 
+
+
+
+
+#
+# main .... sort of
+#
+
+# get the event numbers and names from the devel header file
+find_errors  X.h $X11_NAMES
+LAST_ERROR=`cat $X11_NAMES | grep -v Extension | tail -1 | awk 'BEGIN {FS="="} ; { print $1}' | sed 's,[ ]*,,g' `
+
+
+
+#
+# positive tests
+#
+# compare Xnee number and names with the system
+for i in `cat $X11_NAMES`
+do
+  ER_=`echo $i | sed 's,\([0-9]*\)=.*,\1,g'`
+  NAME_=`echo $i | sed 's,[0-9]*=\([a-zA-Z0-9]*\),\1,g'`
+
+  XNEE_ER=`$XNEE $LONG_ARG $NAME_`
+  STATUS=$?
+  check_retval $STATUS 0 
+  
+
+  XNEE_ER_NAME=`$XNEE $LONG_ARG $ER_`
+  STATUS=$?
+  check_retval $STATUS 0 
+  compare_data $ER_ $NAME_  $XNEE_ER $XNEE_ER_NAME
+
+done
+
+
+
+
+
+# compare Xnee number and names with the system
+for i in `cat $X11_NAMES`
+do
+  ERR_=`echo $i | sed 's,\([0-9]*\)=.*,\1,g'`
+  NAME_=`echo $i | sed 's,[0-9]*=\([a-zA-Z0-9]*\),\1,g'`
+
+  XNEE_ERR=`$XNEE $SHORT_ARG $NAME_`
+  STATUS=$?
+  check_retval $STATUS 0 
+
+  XNEE_ERR_NAME=`$XNEE $SHORT_ARG $ERR_`
+  STATUS=$?
+  check_retval $STATUS 0 
+
+  compare_data $ERR_ $NAME_  $XNEE_ERR $XNEE_ERR_NAME
+done
+
+
+
+
+# loop through all errors and run them in a Xnee renaming pipe 
+# Make sure that 2 == 2   
+# after this has been done:  2 --xnee--> BadFont --xnee--> 2
+# loop from 1 to BadImplementation
+
+TMP=1
+while [ "$TMP" != "$LAST_ERROR" ];
+do
+  TOTAL_TEST=`expr $TOTAL_TEST + 1`
+  REC=`$XNEE $LONG_ARG  $TMP | xargs $XNEE $SHORT_ARG  `
+  STATUS=$?
+  check_retval $STATUS 0 
+  if [ "$REC" != "$TMP" ];
+      then
+      log " unexpected result: using  \"$XNEE $LONG_ARG  $TMP | $XNEE $SHORT_ARG $TMP\""
+      log " got: $REC ..... wanted: $TMP"
+      ERR_TEST=`expr $ERR_TEST + 1`
+  else
+    SUCC_TEST=`expr $SUCC_TEST + 1`
+  fi
+
+
+  TOTAL_TEST=`expr $TOTAL_TEST + 1`
+  REC=`$XNEE $SHORT_ARG  $TMP | xargs $XNEE $LONG_ARG  `
+  STATUS=$?
+  check_retval $STATUS 0 
+  if [ "$REC" != "$TMP" ];
+      then
+      log " unexpected result: using  \"$XNEE $SHORT_ARG  $TMP | $XNEE $LONG_ARG $TMP\""
+      log " got: $REC ..... wanted: $TMP"
+      ERR_TEST=`expr $ERR_TEST + 1`
+  else
+    SUCC_TEST=`expr $SUCC_TEST + 1`
+  fi
+  
+
+  
+  TMP=`expr $TMP + 1`
+done
+
+
+
+#
+# negative tests
+#
+for i in 0=bartok    -12=bela      1=allan 67=pettersson 127=dimitri 200=shostakovich
+do
+  ERR_=`echo $i | sed 's,\([-0-9]*\)=.*,\1,g'`
+  NAME_=`echo $i | sed 's,[-0-9]*=\([a-zA-Z0-9]*\),\1,g'`
+
+  XNEE_ERR=`$XNEE $LONG_ARG $NAME_`
+  STATUS=$?
+  check_retval_false $STATUS 0
+  if [ "$XNEE_ERR" != "" ];
+      then
+      ERR_TEST=`expr $ERR_TEST + 1`
+  else
+      SUCC_TEST=`expr $SUCC_TEST + 1`
+  fi
+  TOTAL_TEST=`expr $TOTAL_TEST + 1`
+
+  XNEE_ERR_NAME=`$XNEE $LONG_ARG $ERR_`
+  STATUS=$?
+  check_retval_false $STATUS 0
+  if [ "$XNEE_ERR_NAME" != "" ];
+      then
+      ERR_TEST=`expr $ERR_TEST + 1`
+  else
+      SUCC_TEST=`expr $SUCC_TEST + 1`
+  fi
+  TOTAL_TEST=`expr $TOTAL_TEST + 1`
+  
+
+  # ###
+  # Xnee returns no value so we can't compare
+  # ### 
+  #  compare_data_false $ERR_ $NAME_  $XNEE_ERR $XNEE_ERR_NAME 
+
+
+  XNEE_ERR=`$XNEE $SHORT_ARG $NAME_`
+  STATUS=$?
+  check_retval_false $STATUS 0
+  if [ "$XNEE_ERR" != "" ];
+      then
+      ERR_TEST=`expr $ERR_TEST + 1`
+  else
+      SUCC_TEST=`expr $SUCC_TEST + 1`
+  fi
+  TOTAL_TEST=`expr $TOTAL_TEST + 1`
+  
+
+
+  XNEE_ERR_NAME=`$XNEE $SHORT_ARG $ERR_`
+  STATUS=$?
+  check_retval_false $STATUS 0
+  if [ "$XNEE_ERR_NAME" != "" ];
+      then
+      ERR_TEST=`expr $ERR_TEST + 1`
+  else
+      SUCC_TEST=`expr $SUCC_TEST + 1`
+  fi
+  TOTAL_TEST=`expr $TOTAL_TEST + 1`
+
+
+
+# ###
+# Xnee returns no value so we can't compare
+# ### 
+#  compare_data_false $ERR_ $NAME_  $XNEE_ERR $XNEE_ERR_NAME 
+done
+
+
+
+\rm -f $X11_NAMES
+
+result_log $MYNAME 
+exit
---
+++ xnee/test/scripts/options/print-error-names.sh
@@ -0,0 +1,194 @@
+#/bin/sh
+#
+#
+# This file is generated from new_script.sh
+#
+#
+# Filename:   opt_scripts/print-error-names.sh
+# Generated:  Sun Mar 16 21:57:36 CET 2003
+# 
+#
+#  Test the Xnee option
+#       --print-error-names
+#  By reading up X11 errors (name and number) from
+#  the header file X.h. These values are compared
+#  with the output from Xnee 
+#
+
+ 
+# name of myself
+MYNAME=print-error-names.sh
+ 
+# Arguments to test (short and long)
+export LONG_ARG=--print-error-names
+export SHORT_ARG=-pens
+ 
+# source useful funs
+if [ ! -f etc/base_funs ]; 
+then 
+    echo "Can't find the file: base_funs"  
+    echo "Start me up in the xnee test dir "
+    exit
+else 
+    . etc/base_funs
+fi 
+ 
+#init_test $MYNAME
+parse_me "$*"
+
+#######################################################################
+
+
+XNEE_NAMES=/tmp/xnee_arg.tmp
+X11_NAMES=/tmp/xnee_errors.tmp
+ 
+
+
+
+#
+#
+#  compare_error
+#
+#
+#  Compare if the the name,number pair given
+#  as argument is the same as given by Xnee --print-error-names
+#  
+# 
+compare_error()
+{
+    EXP_ERR=$1
+    EXP_ERR_NAME=$2
+    MSG=$3
+
+
+    TOTAL_TEST=`expr $TOTAL_TEST + 1`
+
+    # Xnee's number,name pair is located in the file $XNEE_NAMES
+    REC_ERR=`grep -w $EXP_ERR           $XNEE_NAMES | awk '{ print $1}'  | sed 's,[ ],,g'`
+    REC_ERR_NAME=`grep -w $EXP_ERR_NAME $XNEE_NAMES | awk ' { print $2}' | sed 's,[ ],,g'`
+    verbose "verifying error names and numbers"
+    verbose " \"$EXP_ERR\"==\"$REC_ERR\""
+    verbose " \"$EXP_ERR_NAME\"==\"$REC_ERR_NAME\""
+
+    if [ "$REC_ERR" != "$EXP_ERR" ];
+        then
+        log " unexpected result: $MSG"
+        ERR_TEST=`expr $ERR_TEST + 1`
+        return 1
+    fi
+    if [ "$REC_ERR_NAME" != "$EXP_ERR_NAME" ];
+        then
+        log " unexpected result: $MSG"
+        ERR_TEST=`expr $ERR_TEST + 1`
+        return 1
+    fi
+    verbose "success...."
+    SUCC_TEST=`expr $SUCC_TEST + 1`
+}
+
+
+#
+#
+#  compare_error_false
+#
+#
+# 
+compare_error_false()
+{
+    EXP_ERR=$1
+    EXP_ERR_NAME=$2
+    MSG=$3
+
+
+    TOTAL_TEST=`expr $TOTAL_TEST + 1`
+
+    # Xnee's number,name pair is located in the file $XNEE_NAMES
+    REC_ERR=`grep -w $EXP_ERR           $XNEE_NAMES | awk '{ print $1}'  | sed 's,[ ],,g'`
+    REC_ERR_NAME=`grep -w $EXP_ERR_NAME $XNEE_NAMES | awk ' { print $2}' | sed 's,[ ],,g'`
+    verbose "verifying error names and numbers"
+    verbose " \"$EXP_ERR\"==\"$REC_ERR\""
+    verbose " \"$EXP_ERR_NAME\"==\"$REC_ERR_NAME\""
+
+    if [ "$REC_ERR" = "$EXP_ERR" ];
+	then
+	log " unexpected result: $MSG"
+	ERR_TEST=`expr $ERR_TEST + 1`
+	return 1
+    fi
+    if [ "$REC_ERR_NAME" = "$EXP_ERR_NAME" ];
+	then
+	log " unexpected result: $MSG"
+	ERR_TEST=`expr $ERR_TEST + 1`
+	return 1
+    fi
+    verbose "success...."
+    SUCC_TEST=`expr $SUCC_TEST + 1`
+}
+
+
+
+#
+# main .... sort of
+#
+
+# get the error numbers and names from the devel header file
+find_errors  X.h $X11_NAMES
+
+LAST_ERROR=`cat $X11_NAMES | grep -v Extension | tail -1 | awk 'BEGIN {FS="="} ; { print $1}' | sed 's,[ ]*,,g' `
+echo "LAST_ERROR=$LAST_ERROR"
+
+echo "start 1"
+
+#
+# positive test
+#
+#get the error numbers and names from Xnee
+$XNEE $LONG_ARG | grep -e "^[0-9]" >  $XNEE_NAMES
+STATUS=$?
+# compare Xnee number and names with the system
+for i in `cat $X11_NAMES`
+do
+  EV_=`echo $i | sed 's,\([0-9]*\)=.*,\1,g'`
+  NAME_=`echo $i | sed 's,[0-9]*=\([a-zA-Z0-9]*\),\1,g'`
+  if [ "$NAME_" = "$LAST_ERROR" ];
+      then
+      verbose " -- comparing (false): \"$EV_\" \"$NAME_\" "
+      compare_error_false $EV_ $NAME_ "$EV_ $NAME_ "
+  else
+      verbose " -- comparing: \"$EV_\" \"$NAME_\" "
+      compare_error $EV_ $NAME_ "$EV_ $NAME_ "
+  fi
+done
+check_retval $STATUS 0 
+
+result_log $MYNAME 
+
+
+result_log $MYNAME 
+echo "start 2"
+
+
+
+# positive test
+$XNEE $SHORT_ARG | grep -e "^[0-9]" > $XNEE_NAMES.2
+STATUS=$?
+
+diff $XNEE_NAMES $XNEE_NAMES.2>/dev/null 1>/dev/null
+
+TOTAL_TEST=`expr $TOTAL_TEST + 1`
+if [ "$?" != "0" ] ; 
+    then
+    echo " xnee $SHORT_ARG differs from $LONG_ARG"
+    ERR_TEST=`expr $ERR_TEST + 1`
+else
+    SUCC_TEST=`expr $SUCC_TEST + 1`
+fi
+check_retval $STATUS 0 "$STATUS 0"
+
+
+\rm -f $X11_NAMES
+\rm -f $XNEE_NAMES $XNEE_NAMES.2
+
+echo "start 3"
+
+result_log $MYNAME 
