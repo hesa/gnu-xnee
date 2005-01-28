cb0c00cc38045b63e1c1d1f950e1abb6ef3f70a3
hesa <hesa>
2005-01-28 22:41:28 +0000

    File for testing script part of Xnee

---
+++ cnee/test/scripts/scripting/gen_test.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+
+FAKE_MOTION="fake-motion"
+FAKE_KEY="fake-key"
+XNEE=./Xnee-1.08.95a/cnee/src/cnee 
+XNEE_ARGS="--replay --file stdin"
+
+TMP_X=0
+TMP_Y=0
+CTR=0
+
+SLEEP_DELAY=0
+#get_in_position
+
+
+
+
+
+function move_rel_square_sub2()
+{
+    DIR=$1
+    RELATIVE=$2
+    MSLEEP=$3
+    SQUARE_SIZE=$4
+    PIXEL_TO_MOVE=$5
+
+    CTR=0
+    if  [ "$DIR" == "x" ];
+	then
+	TMP_Y=0
+	while [ "$CTR" != "$SQUARE_SIZE" ]
+	  do
+	  CTR=$(( $CTR + 1 ))
+	  TMP_X=$PIXEL_TO_MOVE 
+	  printf "%s   x=%s%.3d y=%s%.3d msecs=%d \n" $FAKE_MOTION $RELATIVE $TMP_X $RELATIVE  $TMP_Y $MSLEEP
+	done 
+    else
+	TMP_X=0
+	while [ "$CTR" != "$SQUARE_SIZE" ]
+	  do
+	  CTR=$(( $CTR + 1 ))
+	  TMP_Y=$PIXEL_TO_MOVE
+	  printf "%s   x=%s%.3d y=%s%.3d msecs=%d \n" $FAKE_MOTION $RELATIVE  $TMP_X  $RELATIVE  $TMP_Y $MSLEEP
+	done 
+    fi
+}
+
+function move_square_sub2()
+{
+    DIR=$1
+    SIGN=$2
+    MSLEEP=$3
+    SQUARE_SIZE=$4
+    PIXEL_TO_MOVE=$5
+
+    CTR=0
+    if  [ "$DIR" == "x" ];
+	then
+	while [ "$CTR" != "$SQUARE_SIZE" ]
+	  do
+	  CTR=$(( $CTR + 1 ))
+	  TMP_X=$(( $TMP_X $SIGN $PIXEL_TO_MOVE ))
+	  printf "%s   x=%.3d y=%.3d msecs=%d \n" $FAKE_MOTION $TMP_X   $TMP_Y $MSLEEP
+	done 
+    else
+	while [ "$CTR" != "$SQUARE_SIZE" ]
+	  do
+	  CTR=$(( $CTR + 1 ))
+	  TMP_Y=$(( $TMP_Y $SIGN $PIXEL_TO_MOVE ))
+	  printf "%s   x=%.3d y=%.3d msecs=%d \n" $FAKE_MOTION   $TMP_X   $TMP_Y $MSLEEP
+	done 
+    fi
+}
+
+function move_square_sub1()
+{
+    MSLEEP=$1
+    SQUARE_SIZE=$2
+    PIXEL_TO_MOVE=$3
+    RELATIVE=$4
+
+    move_square_sub2 "x" "+" "$MSLEEP" "$SQUARE_SIZE" "$PIXEL_TO_MOVE"  "$RELATIVE"
+    sleep $SLEEP_DELAY
+    move_square_sub2 "y" "+" $MSLEEP $SQUARE_SIZE $PIXEL_TO_MOVE  "$RELATIVE"
+    sleep $SLEEP_DELAY
+    move_square_sub2 "x" "-" $MSLEEP $SQUARE_SIZE $PIXEL_TO_MOVE "$RELATIVE"
+    sleep $SLEEP_DELAY
+    move_square_sub2 "y" "-" $MSLEEP $SQUARE_SIZE $PIXEL_TO_MOVE "$RELATIVE"
+    sleep $SLEEP_DELAY
+}
+
+function move_rel_square_sub1()
+{
+    MSLEEP=$1
+    SQUARE_SIZE=$2
+    PIXEL_TO_MOVE=$3
+
+    move_rel_square_sub2 "x" "+" "$MSLEEP" "$SQUARE_SIZE" "$PIXEL_TO_MOVE" 
+    sleep $SLEEP_DELAY
+    move_rel_square_sub2 "y" "+" $MSLEEP $SQUARE_SIZE $PIXEL_TO_MOVE 
+    sleep $SLEEP_DELAY
+    move_rel_square_sub2 "x" "-" $MSLEEP $SQUARE_SIZE $PIXEL_TO_MOVE 
+    sleep $SLEEP_DELAY
+    move_rel_square_sub2 "y" "-" $MSLEEP $SQUARE_SIZE $PIXEL_TO_MOVE 
+    sleep $SLEEP_DELAY
+}
+
+function move_square()
+{
+    MSLEEP=$1
+    SQUARE_SIZE=$2
+    PIXEL_TO_MOVE=$3
+
+    move_square_sub1  $MSLEEP $SQUARE_SIZE $PIXEL_TO_MOVE "0" | $XNEE $XNEE_ARGS 
+}
+
+function move_rel_square()
+{
+    MSLEEP=$1
+    SQUARE_SIZE=$2
+    PIXEL_TO_MOVE=$3
+
+    move_rel_square_sub1  $MSLEEP $SQUARE_SIZE $PIXEL_TO_MOVE  | $XNEE $XNEE_ARGS 
+}
+
+
+function fake_string_sub()
+{
+    STRING=$1
+    MSEC=$2
+    LAST="${#STRING}"
+    CTR=0
+
+    while [  "$CTR" != "$LAST" ]
+      do
+      CHAR=${STRING:$CTR:1}
+      if [ "$CHAR" = " " ]
+	  then
+	  CHAR=XK_space
+      fi
+
+      echo "$FAKE_KEY key=$CHAR msecs=$MSEC"
+      CTR=$(( $CTR + 1 ))
+    done  
+    echo "$FAKE_KEY key=XK_Return msecs=$MSEC "
+}
+
+function fake_string()
+{
+    fake_string_sub "$1"  $2 | $XNEE   $XNEE_ARGS 
+}
+
+
+function fake_button_sub()
+{
+    for i in 1 2 3
+    do
+      echo "fake-button button=$i msecs=1000"
+    done
+}
+
+function fake_buttons()
+{
+    fake_button_sub |  $XNEE   $XNEE_ARGS 
+}
+
+function grab_window()
+{
+    echo "fake-key-press key=Alt_L msecs=100"
+    echo "fake-button-press button=1 msecs=100"
+}
+
+function ungrab_window()
+{
+    echo "fake-button-release button=1 msecs=100"
+    echo "fake-key-release key=Alt_L msecs=100"
+}
+
+function move_win_sub()
+{
+    grab_window
+
+    sleep 1
+    move_rel_square 1 100 1 
+    sleep 1
+    
+    ungrab_window
+}
+
+function move_window()
+{
+    move_win_sub   |  $XNEE   $XNEE_ARGS 
+}
+
+
+
+
+#move_square 1 100 1 
+#move_square 1 400 1
+#move_square 1 800 1
+
+
+
+#fake_string "This is a test and a hard place" 10
+fake_string "ls -lall" 200
+
+#fake_buttons 
+
+#move_window
