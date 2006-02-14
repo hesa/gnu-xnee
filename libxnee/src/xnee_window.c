7ef2bfa008bea8e0aeda5cff55d506067a71953d
hesa <hesa>
2006-02-14 09:14:39 +0000

    new file

--- libxnee/src/xnee_window.c
+++ libxnee/src/xnee_window.c
@@ -22,10 +22,18 @@
  * MA  02110-1301, USA.                                              
  ****/
 
+#include <X11/Xlib.h>
+
 #include "libxnee/xnee.h"
 #include "libxnee/xnee_window.h"
 #include "libxnee/print.h"
 
+/*
+ *  In this file the following two words are frequent enough
+ *  to let their usage be explained:
+ *     session   - information read from session file
+ *     received  - information as received during replay from RECORD
+ */ 
 
 #define XNEE_WINDOW_BUFFER_SIZE 10
 static int received_index = 0;
@@ -34,32 +42,95 @@ static int session_index  = 0;
 xnee_win_pos received_windows[XNEE_WINDOW_BUFFER_SIZE];
 xnee_win_pos session_windows[XNEE_WINDOW_BUFFER_SIZE];
 
+static int
+xnee_window_print_lists(void)
+{
+  int i ; 
+
+  fprintf (stderr,"Session\n");
+  for (i=0; ( i<session_index ) && (i<XNEE_WINDOW_BUFFER_SIZE ) ;i++)
+    {
+      fprintf (stderr,"  %d %d %d\n",
+	       session_windows[i].window,
+	       session_windows[i].x,
+	       session_windows[i].y);
+    }
+  fprintf (stderr,"Received\n");
+  for (i=0;( i<received_index ) && (i<XNEE_WINDOW_BUFFER_SIZE ) ;i++)
+    {
+      fprintf (stderr,"  %d %d %d\n",
+	      received_windows[i].window,
+	      received_windows[i].x,
+	      received_windows[i].y);
+    }
+}
+
+
 int
-xnee_window_add_received(xnee_data *xd, 
-			 xnee_win_pos *xwp)
+xnee_window_add_attribute_impl(xnee_data    *xd, 
+			       XWindowAttributes *attributes,
+			       Window win,
+			       int where)
+{
+  xnee_win_pos  xwp;
+
+  xwp.x      = attributes->x;
+  xwp.y      = attributes->y;
+  xwp.width  = attributes->width;
+  xwp.height = attributes->height;
+  xwp.window = win;
+  
+  return xnee_window_add_impl(xd, &xwp, where); 
+}
+
+
+int
+xnee_window_add_impl(xnee_data    *xd, 
+		     xnee_win_pos *xwp,
+		     int where)
 {
   xnee_win_pos *ptr;
+  int index ;
 
-  printf ("xnee_window_add_received \n");
-  xnee_verbose ((xd, "---> xnee_window_add_received\n"));
-  if ( received_index >= XNEE_WINDOW_BUFFER_SIZE )
+  xnee_verbose((xd, "---> xnee_window_add_impl\n"));
+
+  switch ( where )
+   {
+   case XNEE_WINDOW_RECEIVED:
+      index = received_index;
+      ptr = &received_windows[received_index];
+      break;
+   case XNEE_WINDOW_SESSION:
+      index = session_index;
+      ptr = &session_windows[session_index];
+      break;
+   default:
+     return XNEE_WINDOW_POS_ADJ_ERROR;
+   }
+
+
+  if ( index >= XNEE_WINDOW_BUFFER_SIZE )
     {
-      xnee_verbose ((xd, "<--- xnee_window_add_received   errror\n"));
+      xnee_verbose ((xd, "<--- xnee_window_add_impl ... error\n"));
       return XNEE_WINDOW_POS_ADJ_ERROR;
     }
-
-  ptr = &received_windows[received_index];
 
   ptr->x      = xwp->x;
   ptr->y      = xwp->y;
   ptr->height = xwp->height;
   ptr->width  = xwp->width;
- 
+  ptr->window = xwp->window;
+  
+  if ( where == XNEE_WINDOW_RECEIVED )
+    {
+      received_index++;
+    }
+  else
+    {
+      session_index++;
+    }
 
-  received_index++;
-
-  printf ( "<--- xnee_window_add_received  %dx%d\n", ptr->x, ptr->y);
-  xnee_verbose ((xd, "<--- xnee_window_add_received  %dx%d\n", ptr->x, ptr->y));
+  xnee_verbose((xd, "<--- xnee_window_add_impl\n"));
   return XNEE_OK;
 }
 
@@ -70,15 +141,11 @@ xnee_window_add_read(xnee_data    *xd,
 {
   xnee_win_pos *ptr;
 
-  xnee_verbose ((xd, "---> xnee_window_add_read\n"));
-
-  printf ("xnee_window_add_read: ");
-  printf ("  %dx%d+%d+%d\n",
-	  xwp->x,xwp->y,xwp->width,xwp->height);
+  xnee_verbose((xd, "---> xnee_window_add_read\n"));
 
   if ( session_index >= XNEE_WINDOW_BUFFER_SIZE )
     {
-      xnee_verbose ((xd, "<--- xnee_window_add_read ... error\n"));
+      xnee_verbose((xd, "<--- xnee_window_add_read ... error\n"));
       return XNEE_WINDOW_POS_ADJ_ERROR;
     }
 
@@ -92,7 +159,7 @@ xnee_window_add_read(xnee_data    *xd,
   
   session_index++;
 
-  xnee_verbose ((xd, "<--- xnee_window_add_read\n"));
+  xnee_verbose((xd, "<--- xnee_window_add_read\n"));
   return XNEE_OK;
 }
 
@@ -100,7 +167,53 @@ xnee_window_add_read(xnee_data    *xd,
 int
 xnee_window_try_move(xnee_data *xd)
 {
-  ;
+  int ret = XNEE_OK;
+  xnee_win_pos *sess_ptr;
+  xnee_win_pos *rec_ptr;
+
+
+  if (xnee_is_verbose(xd))
+    {
+      xnee_window_print_lists();
+    }
+
+  /*
+   * If we haven't read (from file) and received (from RECORD)
+   * then there we lack info on how to move the new window...
+   * return....
+   */
+  if ( (session_index==0) || (received_index==0) )
+    {
+      return XNEE_OK;
+    }
+
+  /*
+   * OK, we have read info on saved window and we have
+   * a window to apply that info on, let's do it.
+   */
+  sess_ptr =  &session_windows[session_index-1];
+  rec_ptr  =  &received_windows[received_index-1];
+
+  if ( rec_ptr->window != 0 )
+    {
+      xnee_verbose((xd, " XMoveWindow(%d,%d,%d,%d)\n", 
+		    xd->control,
+		    rec_ptr->window,
+		    sess_ptr->x,
+		    sess_ptr->y));
+      XMoveWindow(xd->control,
+		  rec_ptr->window,
+		  sess_ptr->x,
+		  sess_ptr->y);
+
+      session_index--;
+      received_index--;
+    }
+  else
+    {
+      return XNEE_WINDOW_POS_ADJ_ERROR;
+    }
+  return ret;
 }
 
 
