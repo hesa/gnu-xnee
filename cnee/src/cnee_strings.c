0bf9235785886037ae2b1852eb56f2e96b6cd111
hesa <hesa>
2006-03-31 18:22:11 +0000

    Removed string constants

--- cnee/src/cnee_strings.c
+++ cnee/src/cnee_strings.c
@@ -23,69 +23,8 @@
  * MA  02110-1301, USA.                                              
  ****/
 
-#include <stdio.h>
-#include "cnee_strings.h"
 
-const char* CNEE_FILE_OPTION_LONG              = "file";
-const char* CNEE_FILE_OPTION_SHORT             = "f";
 
-const char* CNEE_HELP_OPTION_LONG              = "felp";
-const char* CNEE_HELP_OPTION_SHORT             = "h";
 
-const char* CNEE_FLAGS_OPTION_LONG             = "flags";
-const char* CNEE_FLAGS_OPTION_SHORT            = NULL;
 
-const char* CNEE_PROJECT_OPTION_LONG           = "project";
-const char* CNEE_PROJECT_OPTION_SHORT          = "pr";
-
-const char* CNEE_VERSION_OPTION_LONG           = "version";
-const char* CNEE_VERSION_OPTION_SHORT          = "V";
-
-const char* CNEE_RECORD_OPTION_LONG            = "record";
-const char* CNEE_RECORD_OPTION_SHORT           = "rec";
-
-const char* CNEE_REPLAY_OPTION_LONG            = "replay";
-const char* CNEE_REPLAY_OPTION_SHORT           = "rep";
-
-const char* CNEE_RETYPE_FILE_OPTION_LONG       = "retype";
-const char* CNEE_RETYPE_FILE_OPTION_SHORT      = NULL;
-
-const char* CNEE_TYPE_HELP_OPTION_LONG         = "type-help";
-const char* CNEE_TYPE_HELP_OPTION_SHORT        = NULL;
-
-const char* CNEE_GEN_MANPAGE_OPTION_LONG       = "manpage";
-const char* CNEE_GEN_MANPAGE_OPTION_SHORT      = NULL;
-
-const char* CNEE_GEN_INFOPAGE_OPTION_LONG      = "infopage";
-const char* CNEE_GEN_INFOPAGE_OPTION_SHORT     = NULL;
-
-const char* CNEE_WRITE_SETTINGS_OPTION_LONG    = "write-settings file";
-const char* CNEE_WRITE_SETTINGS_OPTION_SHORT   = NULL;
-
-const char* CNEE_PRINT_SETTINGS_OPTION_LONG    = "print-settings";
-const char* CNEE_PRINT_SETTINGS_OPTION_SHORT   = "ps";
-
-const char* CNEE_PRINT_E_NAMES_OPTION_LONG     = "print-event-names";
-const char* CNEE_PRINT_E_NAMES_OPTION_SHORT    = "pens";
-
-const char* CNEE_PRINT_E_NAME_OPTION_LONG      = "print-event-name";
-const char* CNEE_PRINT_E_NAME_OPTION_SHORT     = "pen";
-
-const char* CNEE_PRINT_ERR_NAMES_OPTION_LONG   = "print-error-names";
-const char* CNEE_PRINT_ERR_NAMES_OPTION_SHORT  = "perns";
-
-const char* CNEE_PRINT_ERR_NAME_OPTION_LONG    = "print-error-name";
-const char* CNEE_PRINT_ERR_NAME_OPTION_SHORT   = "pern";
-
-const char* CNEE_PRINT_REP_NAMES_OPTION_LONG   = "print-reply-names";
-const char* CNEE_PRINT_REP_NAMES_OPTION_SHORT  = "pren";
-
-const char* CNEE_PRINT_REQ_NAMES_OPTION_LONG   = "print-request-names";
-const char* CNEE_PRINT_REQ_NAMES_OPTION_SHORT  = "prns";
-
-const char* CNEE_PRINT_REQ_NAME_OPTION_LONG    = "print-request-name";
-const char* CNEE_PRINT_REQ_NAME_OPTION_SHORT   = "prn";
-
-const char* CNEE_PRINT_DATA_NAMES_OPTION_LONG  = "print-data-names";
-const char* CNEE_PRINT_DATA_NAMES_OPTION_SHORT = "pdn";
 
