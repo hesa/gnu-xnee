9a93f2512d054fd2d542e661bb16a84331ee04a7
hesa <hesa>
2008-11-28 23:22:08 +0000

    Xvfb used to test Work around for Xvfb (freecolormap) bug? xterm, xwininfo, xdpyinfo, xset used to generate x11 data

--- build/profile.sh
+++ build/profile.sh
@@ -8,7 +8,6 @@ FMT=pdf
 PATH=$PATH:/opt/gprof2dot
 DATE=$(date '+%Y%m%d')
 
-
 if [ "$1" = "" ]
 then
     BASE_DEST_DIR=/tmp/xnee-temp
@@ -16,33 +15,116 @@ else
     BASE_DEST_DIR=$1
 fi
 
-DEST_DIR=$BASE_DEST_DIR/profile/$DATE/x86
-mkdir -p $DEST_DIR
 
-mkdir doc/profile
+init_test()
+{
+    for i in $(find -name "gmon.out*" )
+    do
+	rm $i
+    done
+#
+#  Setup virtual X environment
+#
+    Xvfb -ac -screen 0 640x480x16 :32  2> /dev/null  > /dev/null &
+    XVFB_PID=$!
+    
+    
+    DISPLAY=:32
+    sleep 2
+    
 
-./configure \
-  --disable-gui \
-  --disable-gnome-applet \
-  --disable-doc          \
-  --enable-gprof               && \
-make clean all check           && \
-cd cnee/src                        && \
-mv ../test/gmon.out* ./ ; \
-rm -f gmon.out.txt  gmon.sum       && \
-gprof -s cnee gmon.ou*             && \
-gprof cnee gmon.sum > gmon.out.txt && \
-gprof2dot.pl    \
-   --transformer transformer.pl \
-   --output cnee.dot --cluster-by-scope --merge-overloaded-functions \
-   gmon.out.txt                && \
-dot -T$FMT -o cnee.$FMT cnee.dot &&\
-mv cnee.$FMT ../../doc/profile && \
-cd ../.. && \
-cp doc/profile/cnee.$FMT $DEST_DIR/cnee-call-graph.$FMT && \
-cp cnee/src/gmon.out.txt $DEST_DIR/ && \
-echo "OK :)"
+    DEST_DIR=$BASE_DEST_DIR/profile/$DATE/x86
+    mkdir -p $DEST_DIR
+    
+}
+
+gen_x11_data()
+{
+    TMP_CNT=0 ; 
+    while (true) ; 
+    do 
+
+	echo "Fake X11 data round: $TMP_CNT"
+	xterm -e sleep 1  2> /dev/null  > /dev/null; 
+	sleep 0
+	xdpyinfo 2> /dev/null > /dev/null
+	sleep 0
+	xwininfo -root 2> /dev/null > /dev/null
+	sleep 0
+	xset r on 2> /dev/null > /dev/null
+
+	# check if we've lost the X server
+	RET=$? ; 
+
+	if [ $RET -ne 0 ] ; 
+	then
+	    echo  "NOK, date: $(date)   time=$TMP";  
+	    break ; 
+	fi ; 
+
+	TMP_CNT=$(( $TMP_CNT + 1 )) ; 
+
+	if [ $TMP_CNT -gt 50 ] ; 
+	then
+	    echo  " ok, we should be done generating X11 data";  
+	    break ; 
+	fi ; 
+	
+    done
 
 
+}
+
+
+
+prepare_sw_for_test()
+{
+    ./configure \
+	--disable-gui \
+	--disable-gnome-applet \
+	--disable-doc          \
+	--enable-gprof         && \
+	make clean all           
+    RET=$?
+    return $RET
+}
+
+do_the_tests()
+{
+    make  check           && \
+	cd cnee/src                        && \
+	mv ../test/gmon.out* ./ ; 
+
+	rm -f gmon.out.txt  gmon.sum       && \
+	gprof -s cnee gmon.ou*             && \
+	gprof cnee gmon.sum > gmon.out.txt && \
+	gprof2dot.pl    \
+	--transformer transformer.pl \
+	--output cnee.dot --cluster-by-scope --merge-overloaded-functions \
+	gmon.out.txt                && \
+	dot -T$FMT -o cnee.$FMT cnee.dot && \
+        mv cnee.$FMT ../../doc/profile && \
+	cd ../.. && \
+	cp doc/profile/cnee.$FMT $DEST_DIR/cnee-call-graph.$FMT && \
+	cp cnee/src/gmon.out.txt $DEST_DIR/ && \
+	echo "OK :)"
+}
+
+
+close_down()
+{
+    sleep 1 
+    kill    $XVFB_PID
+    sleep 1 
+    kill -9 $XVFB_PID
+    sleep 1
+}
+
+
+init_test
+#prepare_sw_for_test
+gen_x11_data&
+do_the_tests
+close_down
 
 #mogrify -rotate 90 doc/profile/cnee.$FMT &&\
