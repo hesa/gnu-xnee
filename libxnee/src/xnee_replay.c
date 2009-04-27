3c5d9071393b0e5d911fc818bdb49b053655a843
hesa <hesa>
2009-04-27 21:25:01 +0000

    (c) year updated

--- libxnee/src/xnee_replay.c
+++ libxnee/src/xnee_replay.c
@@ -4,7 +4,8 @@
  * Xnee enables recording and replaying of X protocol data           
  *                                                                   
  *        Copyright (C) 1999, 2000, 2001, 2002, 2003
- *                      2004, 2005, 2006, 2007 Henrik Sandklef              
+ *                      2004, 2005, 2006, 2007, 2008
+ *                      2009  Henrik Sandklef              
  *                                                                   
  * This program is free software; you can redistribute it and/or     
  * modify it under the terms of the GNU General Public License       
@@ -229,8 +230,6 @@ xnee_replay_main_loop(xnee_data *xd, int read_mode)
              ret = (int)strlen (ret_str);
           }
 
-	  
-	  
 	  if ( ret == -1 )
 	    {
 	      return XNEE_OK;
@@ -502,7 +501,6 @@ xnee_replay_main_loop(xnee_data *xd, int read_mode)
 	  xnee_verbose((xd, "  <-- Flushed after handled event\n"));
 
 	  ret_str = fgets(tmp, 256, xd->data_file);
-
 	  if (ret_str == NULL)
 	    {
  	      break; 
