52ab95c2a86a01907dbf81775f04feeed40c6f85
hesa <hesa>
2009-12-29 00:29:53 +0000

    First rev.   Test program wrapper, checks for running X

---
+++ libxnee/test/test-wrapper.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+test_program()
+{
+    if [ ! -x $1 ]
+    then
+	echo "Program $1 does not exists, or is not executable"
+	return 1
+    fi
+
+    xdpyinfo >/dev/null 2>&1 
+    if [ "$?" != "0" ]
+    then
+	echo "Problem with X server (prob authorisation, or no X up and running)"
+	echo "   when executing program $1"
+	echo "   .... bailing out"
+	return 1
+    fi
+}
+
+test_program $1
+return 0
+
