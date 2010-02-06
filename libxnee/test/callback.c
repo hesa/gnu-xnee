7278843fbc02158ba13808e056bb815fe4bfe0bd
hesa <hesa>
2010-02-06 12:18:42 +0000

    Casting xd to XPointer to silent warning

--- libxnee/test/callback.c
+++ libxnee/test/callback.c
@@ -15,13 +15,13 @@ int main()
   printf ("Test rec callback\n");
   for (i=0;i<100000;i++)
     {
-      xd->rec_callback(xd, NULL);
+      xd->rec_callback((XPointer)xd, NULL);
     }
 
   printf ("Test rep callback\n");
   for (i=0;i<100000;i++)
     {
-      xd->rep_callback(xd, NULL);
+      xd->rep_callback((XPointer)xd, NULL);
     }
 
 
