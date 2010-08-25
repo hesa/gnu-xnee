520ba855bc6ed23786e934a833bb47f9fad93cf3
hesa <hesa>
2010-08-25 21:23:25 +0000

    Removed unhandled check of return value Added copyright year

--- libxnee/src/xnee.c
+++ libxnee/src/xnee.c
@@ -3,7 +3,8 @@
  *                                                                   
  * Xnee enables recording and replaying of X protocol data           
  *                                                                   
- *   Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
+ *   Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 
+ *                 2006, 2007, 2010
  *                        Henrik Sandklef 
  *                                                                   
  * This program is free software; you can redistribute it and/or     
@@ -99,10 +100,6 @@ xnee_start(xnee_data *xd)
       handled by normal parsing ..... */
    if (xnee_is_syntax_checker(xd))
      {
-       if (ret == XNEE_OK)
-	 {
-	   ;
-	 }
        return XNEE_OK;
      }
 
