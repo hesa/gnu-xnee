95d6c5b84e83bfd34bcd1de37a3d5e5c30be6908
hesa <hesa>
2010-09-13 22:07:59 +0000

    Removed unused old settings Re-enabled clean target

--- build/maemo.sh
+++ build/maemo.sh
@@ -10,10 +10,9 @@ make_makefile()
 {
     export  CC=/home/hesa/opt/NokiaQtSDK/Maemo/4.6.2/targets/fremantle-qt-1014/bin/gcc
 
-#/home/hesa/opt/NokiaQtSDK/Maemo/4.6.2/toolchains/arm-2007q3-51sb6-gdb701-arm-none-linux-gnueabi_linux/arm-none-linux-gnueabi/bin/gcc
     make -f Makefile.cvs && \
       ./configure --disable-gui --disable-doc --disable-gnome-applet --host=arm --disable-xinput2
 }
 
-#clean_up
+clean_up
 make_makefile
