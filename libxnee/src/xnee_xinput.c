11086449bd0b25e891b71d1e8de4415caa54a15a
hesa <hesa>
2010-09-24 07:25:12 +0000

    Human printout of xi events New function to init Xnee's XInput stuff (no device init)

--- libxnee/src/xnee_xinput.c
+++ libxnee/src/xnee_xinput.c
@@ -23,6 +23,7 @@
  ****/
 
 #include "libxnee/xnee.h"
+#include "libxnee/print.h"
 #include "libxnee/xnee_xinput.h"
 
 #define XNEE_XINPUT_PRINT_MASTER_OR_SLAVE(xd, devid, fd)	\
@@ -34,6 +35,16 @@
 
 
 #ifdef XNEE_XINPUT_SUPPORT
+
+int
+xnee_init_xinput(xnee_data *xd)
+{
+  if ( (xd==NULL) || (xd->control==NULL) )
+    {
+      return -1;
+    }
+  xd->xi_data.forced_core_replay = 0;
+}
 
 int
 xnee_init_xinput_devices(xnee_data *xd)
@@ -66,6 +77,7 @@ xnee_init_xinput_devices(xnee_data *xd)
 	
       xd->xi_data.xi_devices[dev->deviceid].name     = strdup(dev->name);
       xd->xi_data.xi_devices[dev->deviceid].deviceid = dev->deviceid;
+      xd->xi_data.xi_devices[dev->deviceid].device        = NULL ;
 
       if (dev->use == XIMasterPointer || dev->use == XIMasterKeyboard)
 	{
@@ -76,10 +88,56 @@ xnee_init_xinput_devices(xnee_data *xd)
 	  xd->xi_data.xi_devices[dev->deviceid].is_slave = 1;
 	}
     }
+  xd->xi_data.nr_of_xi_devices   = nr_of_devices;
 
   XIFreeDeviceInfo(xi_info);
   
   return 0;
+}
+
+XDevice *
+xnee_get_xinput_device(xnee_data *xd, int deviceid)
+{
+  int count;
+  int i;
+  int dev_pointer;
+  XDeviceInfo *dev_info;
+  XDevice *xdevice = NULL ;
+
+  if ( xd->xi_data.xi_devices[deviceid].device == NULL )
+    {
+      if (!(dev_info = XListInputDevices(xd->data, &count)) || !count)
+	{
+	  fprintf(stderr, "Cannot list input devices\n");
+	  return NULL;
+	}
+
+      dev_pointer = 0;
+      for (i = 0; i < count; i++) 
+	{
+	  xnee_verbose((xd, "pointer: %d %d (%d)  [%d %d ]\n", 
+			i, 
+			(int)dev_info[i].id, 
+			(int)deviceid,
+			(int)xd->xi_data.xi_devices[i].deviceid,
+			(int)xd->xi_data.nr_of_xi_devices));
+
+	  if ( dev_info[i].id  == deviceid) 
+	    {
+	      dev_pointer = i; 
+	      xdevice = XOpenDevice(xd->data, dev_info[dev_pointer].id);
+	      xd->xi_data.xi_devices[deviceid].device=xdevice;
+	      break;
+	    }
+	}
+
+      XFreeDeviceList(dev_info);
+    }
+
+
+  
+  /* we may return NULL, so make sure to check */
+  return xd->xi_data.xi_devices[deviceid].device;
 }
 
 int
@@ -223,13 +281,118 @@ xnee_handle_xinput_event(xnee_data * xd,
 	  XNEE_XINPUT_PRINT_MASTER_OR_SLAVE(xd, e->deviceid, out);
 	  fprintf (out, ",%d,%d,%d,0,0,0,%lu,%d,'%s'\n",
 		   sxe.type,
-		   e->valuator0, 
-		   e->valuator1, 
+		   (int)e->valuator0, 
+		   (int)e->valuator1, 
 		   server_time,
 		   /* sxe.time, */
 		   e->deviceid,
 		   xd->xi_data.xi_devices[e->deviceid].name);
 	  
+	  sxe.x = 0;
+	  sxe.y = 0;
+	  sxe.button = 0 ;
+	}
+    }
+  else
+    {
+      return -1;
+    }
+  return XNEE_OK;
+}
+
+int 
+xnee_handle_xinput_event_human(xnee_data * xd, 
+			       int event_type, 
+			       XRecordDatum *xrec_data)
+{
+  static saved_xinput_event sxe;
+  FILE *out ;
+  out = xd->out_file;
+  
+  /*
+   * Are we using Xinput
+   *  AND
+   * the event is a xinput device event
+   */
+  if ( ( xd->xi_data.xinput_event_base > 0 )          &&
+       ( event_type > xd->xi_data.xinput_event_base ) &&
+       ( event_type <= (xd->xi_data.xinput_event_base + 6) ))
+    {
+      int ordinary_event_nr ;
+      deviceKeyButtonPointer *e;
+      e = (deviceKeyButtonPointer *) &xrec_data->event ;
+      
+      
+      ordinary_event_nr = event_type - xd->xi_data.xinput_event_base + 1;
+      sxe.type         =  ordinary_event_nr ;
+      
+      /* printf ("GOT %d | detail: %d  saved=%d\n", ordinary_event_nr, xrec_data->event.u.u.detail, sxe.type); */
+      
+      /* MotionEvent */
+      if ( ordinary_event_nr == MotionNotify)
+	{
+	  sxe.x        = xrec_data->event.u.keyButtonPointer.rootX;
+	  sxe.y        = xrec_data->event.u.keyButtonPointer.rootY;
+	  sxe.time     = xrec_data->event.u.keyButtonPointer.time;
+	  
+	  /* 
+	   * Rest of the data is sent in an Devicevaluator event, 
+	   * store what we have and continue
+	   */
+	}
+      
+      /* ButtonPress / ButtonRelease */
+      else if ( ( ordinary_event_nr ==  ButtonPress) || ( ordinary_event_nr ==  ButtonRelease) )
+	{
+	  XNEE_XINPUT_PRINT_MASTER_OR_SLAVE(xd, e->deviceid, out);
+	}
+      else if ( ( ordinary_event_nr ==  KeyPress) || ( ordinary_event_nr ==  KeyRelease) )
+	{
+	  XNEE_XINPUT_PRINT_MASTER_OR_SLAVE(xd, e->deviceid, out);
+	  fprintf (out, ",%u,0,0,0,%d,0,%lu,%d,%s\n",
+		   ordinary_event_nr,
+		   e->detail ,
+		   sxe.time,
+		   e->deviceid,
+		   xd->xi_data.xi_devices[e->deviceid].name);
+	}
+    }
+  else if ( event_type == xd->xi_data.xinput_event_base)
+    {
+      
+      /*
+       *
+       *  DaviceValuator event
+       *
+       */
+      deviceValuator *e;
+      e = (deviceValuator *) &xrec_data->event ;
+      
+      /*	printf ("deviceValuator: %d %d (%d),  %d, %d (%d %d %d %d %d %d)    sxe.type=%d\n", 
+		e->type, e->deviceid, 	sxe.deviceid,		e->num_valuators, 
+		e->first_valuator, 	e->valuator0, 
+		e->valuator1, e->valuator2, e->valuator3, e->valuator4, e->valuator5,	sxe.type
+		);		*/		
+      if ( sxe.type == MotionNotify )
+	{
+	  if ( e->num_valuators != 2 )
+	    {
+	      printf ("Number of valuators was faulty :(\n");
+	      /*
+	       * HESA HESA HESA
+	       * exit is good when developing
+	       * since it forces an exit on error ..... 
+	       *  FIX LATER
+	       */
+	      exit(1);
+	    }
+	  
+	  XNEE_XINPUT_PRINT_MASTER_OR_SLAVE(xd, e->deviceid, out);
+	  fprintf (out, ",XInput Event=%s  Number=%d %s",
+		   xd->xi_data.xi_devices[e->deviceid].name,		  
+		   sxe.type);
+	  (void)xd->data_fp (xd->out_file,"\n");
+
 	  sxe.x = 0;
 	  sxe.y = 0;
 	  sxe.button = 0 ;
@@ -299,6 +462,12 @@ xnee_xinput_add_devices(xnee_data *xd)
 
 
 #else /*  XNEE_XINPUT_SUPPORT */
+
+int
+xnee_init_xinput(xnee_data *xd)
+{
+  return XNEE_OK;
+}
 
 
 int
