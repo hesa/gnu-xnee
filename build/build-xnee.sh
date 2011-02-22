f2f61994192fe1f53dd5daff71515a15e26ac958
hesa <hesa>
2011-02-22 21:14:42 +0000

    checking for return values

--- build/build-xnee.sh
+++ build/build-xnee.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 make -f Makefile.cvs && ./configure --disable-xinput --disable-doc && make && make check
+RET=$?
 
-echo "Xnee built well on: $(uname -a)"
+if [ "$RET" = "0" ]
+then
+    echo "Xnee built well at $(date) on $(uname -a)"
+else
+    echo "Xnee built failed at $(date) on $(uname -a)"
+fi
