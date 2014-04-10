231c3c7db0b9dc3a7cbe5275859f2d295f6b0426
hesa <hesa>
2014-04-10 17:21:45 +0000

    Devel pkgs for debian

--- build/setup-debian.sh
+++ build/setup-debian.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-sudo apt-get install  texlive-extra-utils texlive-font-utils libxtst-dev texi2pdf gs-common dvipdf texi2html texinfo  imagemagick pkg-config dia  texi2html  
+DOC_PKGS="texlive-extra-utils  libxtst-dev gs-common texi2html texinfo  imagemagick pkg-config dia  texi2html texlive-font-utils" 
+
+DEVEL_PKGS="libtool gcc automake autoconf make libgtk2.0-dev"
+
+my_install() 
+{
+    apt-get install -y  $*
+}
+
+
+my_install $DOC_PKGS
+
+my_install $DEVEL_PKGS
+
